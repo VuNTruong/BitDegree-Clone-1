4a23ec67ecf608a908cdab28b013e261f300b56a
vntruong1999 <56061432+vntruong1999@users.noreply.github.com>
2021-10-29 17:14:36 +0700

    Updated

--- js/script.js
+++ js/script.js
@@ -10,15 +10,24 @@ function onClickOpenHamburgerMenu() {
   document.getElementById("hamburger-menu").classList.toggle("show-1");
 }
 
-// The function to handle event of when width of the window is greater than 768px
+// The function to handle event of when there is a change in layout
 function checkMediaQuery() {
-  // If the inner width of the window is greater than 768px
+  // If the inner width of the window is less than 768px
   if (window.innerWidth < 768) {
     // Get the dropdown menu element based on id
     var dropdownMenu = this.document.getElementById("drop-down-menu");
 
-    // Close the menu by removing it out of the view
+    // Close the menu by removing it away from the view
     dropdownMenu.classList.remove("show");
+  }
+
+  // If the inner width of the window is greater than 768px
+  if (window.innerWidth > 768) {
+    // Get the hamburger menu element based on id
+    var hamburgerMenu = this.document.getElementById("hamburger-menu");
+
+    // Close the menu by removing it away from the view
+    hamburgerMenu.classList.remove("show-1");
   }
 }
 
@@ -27,7 +36,9 @@ window.onclick = function (event) {
   // Check to see if clicked area is outside of the button or not
   if (
     !event.target.matches(".category-filter") &&
-    !event.target.matches(".hamburger-menu-button")
+    !event.target.matches(".hamburger-menu-button") &&
+    !event.target.matches(".category-filter-text") &&
+    !event.target.matches(".category-filter-icon")
   ) {
     // Get the dropdown menu element based on id
     var dropdownMenu = this.document.getElementById("drop-down-menu");
